64739500f6d06ee9c8b93d890fea775e2c34a31b
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-12-03 22:32:27 +0400

    fixed prvate route

--- src/Pages/PrivateRoute/PrivateRoute.jsx
+++ src/Pages/PrivateRoute/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
-import { AuthContext } from "../Providers/AuthProviders";
+import { AuthContext } from "../../Providers/AuthProviders";
 import { Navigate } from "react-router-dom";
+
 
 
 const PrivateRoute = ({children}) => {
