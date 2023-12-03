4c811b670000fe26cc76830ef754fb032d9f48c7
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-12-03 22:49:14 +0400

    partners done

--- src/Pages/Home/AdvertisementSection/AdvertisementSection.jsx
+++ src/Pages/Home/AdvertisementSection/AdvertisementSection.jsx
@@ -11,8 +11,8 @@ const AdvertisementSection = () => {
     },[])
     return (
         <>
-        <h2 className="text-3xl text-sky-600 text-center my-10">Advertisement</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 shadow-xl my-15 mt-20 mx-auto  mt-15">
+        <h2 className="text-4xl text-sky-600 text-center my-10 font-bold">Advertisement</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 shadow-xl my-15 mt-20 mx-auto  mt-15 mb-20">
         {
             add.slice(0, 4).map(advertise => <SingleAdd 
                 key={advertise._id}
--- src/Pages/Home/Partners/Partners.jsx
+++ src/Pages/Home/Partners/Partners.jsx
@@ -7,33 +7,33 @@ import { BiSolidBuildingHouse } from "react-icons/bi";
 const Partners = () => {
     return (
         <div>
-            <h2 className="text-5xl my-10 ml-20 font-bold">Our Partners</h2>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 text-center">
+            <h2 className="text-4xl text-sky-600 my-10 ml-20 font-bold text-center">Our Partners</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 text-center my-10">
                 <div>
                     <RiBuilding2Line className="text-5xl mx-auto"></RiBuilding2Line>
                     <div>
-                    <h2 className="text-3xl">High-Rise</h2>
+                    <h2 className="text-3xl text-sky-600">High-Rise</h2>
                     <p>Real Estate Company</p>
                     </div>
                 </div>
                 <div>
                 <FaBuilding className="text-5xl mx-auto"></FaBuilding>
                     <div>
-                    <h2 className="text-3xl">Mark & Co.</h2>
+                    <h2 className="text-3xl text-sky-600">Mark & Co.</h2>
                     <p>Buildings</p>
                     </div>
                 </div>
                 <div>
                     <TbBuildingCarousel className="text-5xl mx-auto"></TbBuildingCarousel>
                     <div>
-                    <h2 className="text-3xl">Real Estate Experts</h2>
+                    <h2 className="text-3xl text-sky-600">Real Estate Experts</h2>
                     <p>Real Estate Company</p>
                     </div>
                 </div>
                 <div>
                   <BiSolidBuildingHouse className="text-5xl mx-auto"></BiSolidBuildingHouse>
                     <div>
-                    <h2 className="text-3xl">Home Design</h2>
+                    <h2 className="text-3xl text-sky-600">Home Design</h2>
                     <p>Real Estate Company</p>
                     </div>
                 </div>
