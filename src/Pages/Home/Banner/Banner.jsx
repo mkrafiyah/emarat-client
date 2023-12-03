ccf92c0681d81927381cecd8797922208be3cb39
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-12-03 22:40:38 +0400

    banner section done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -10,14 +10,14 @@ const Banner = () => {
                     <p className="mb-5 text-3xl">Innovation</p>
                     <h1 className="mb-5 text-5xl font-bold">Enhanced <br /> Living Experience</h1>
                     <div className="flex gap-8 mb-5">
-                        <div className="flex gap-4 justify-center items-center bg-teal-700 p-5 rounded-lg">
+                        <div className="flex gap-4 justify-center items-center bg-sky-600 p-5 rounded-lg">
                             <p> <FaKey className="text-4xl"></FaKey></p>
                             <div>
                                 <p className="text-3xl font-bold">Prices From</p>
                                 <p className="text-3xl font-bold">$12000.00</p>
                             </div>
                         </div>
-                        <div  className="flex gap-4 justify-center items-center bg-teal-700 p-5 rounded-lg">
+                        <div  className="flex gap-4 justify-center items-center bg-sky-600 p-5 rounded-lg">
                         <p> <FaMobile className="text-4xl"></FaMobile></p>
                             <div>
                             <p className="text-3xl font-bold">Call Today</p>
